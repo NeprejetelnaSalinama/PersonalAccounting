6979c1997376853f577b91fd78d8866490dfe59d
Denisa <DenisaVanackova@gmail.com>
2016-01-11 15:00:19 +0100

    commit before deleting CategoryTypes table and adding Category as property instead

--- DenisAccounting/Controllers/CategoryController.cs
+++ DenisAccounting/Controllers/CategoryController.cs
@@ -6,128 +6,30 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using DenisAccounting.Database;
+using DenisAccounting.Managers;
 using DenisAccounting.Models;
+using AutoMapper;
 
 namespace DenisAccounting.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoryController : BaseController
     {
-        private AccountingContext db = new AccountingContext();
+        private CategoriesManager categoriesManager;
 
-        // GET: Category
-        public ActionResult Index()
+        public CategoryController()
         {
-            var categories = db.Categories.Include(c => c.CategoryType);
-            return View(categories.ToList());
+            categoriesManager = new CategoriesManager(database);
         }
 
-        // GET: Category/Details/5
-        public ActionResult Details(int? id)
+        public ViewResult IncomeIndex()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
+            
 
-        // GET: Category/Create
-        public ActionResult Create()
-        {
-            ViewBag.Id = new SelectList(db.CategoryTypes, "Id", "name");
-            return View();
-        }
+            var categories = categoriesManager.GetCategoriesSelectList();
+            var categoriesModel = categories
+                .Select(Category);
 
-        // POST: Category/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Id,Name")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Categories.Add(category);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Id = new SelectList(db.CategoryTypes, "Id", "name", category.Id);
-            return View(category);
-        }
-
-        // GET: Category/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Id = new SelectList(db.CategoryTypes, "Id", "name", category.Id);
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Id,Name")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(category).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Id = new SelectList(db.CategoryTypes, "Id", "name", category.Id);
-            return View(category);
-        }
-
-        // GET: Category/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Category category = db.Categories.Find(id);
-            db.Categories.Remove(category);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return View(categoriesModel);
         }
     }
 }
--- DenisAccounting/Controllers/OperationController.cs
+++ DenisAccounting/Controllers/OperationController.cs
@@ -6,148 +6,49 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using DenisAccounting.Database;
+using DenisAccounting.Models.Operations;
 using DenisAccounting.Models;
+using DenisAccounting.Managers;
+using AutoMapper;
 
 namespace DenisAccounting.Controllers
 {
-    public class OperationController : Controller
+    public class OperationController : BaseController
     {
-        private AccountingContext db = new AccountingContext();
+        private OperationsManager operationsManager;
+        private CategoriesManager categoriesManager;
 
-        // GET: Operation
-        public ActionResult Index()
-        {
-            var operations = db.Operations.Include(o => o.Category).Include(o => o.Currency);
-            return View(operations.ToList());
+        public OperationController() {
+            operationsManager = new OperationsManager(database);
+            categoriesManager = new CategoriesManager(database);
         }
 
-        // GET: Operation/Details/5
-        public ActionResult Details(int? id)
+        public ViewResult Index()
+        {
+            var operations = operationsManager.getOperations();
+            var operationsModel = operations
+                .Select(Mapper.Map<IndexViewModel>);
+
+            return View(operationsModel);
+        }
+
+        public ActionResult Details(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Operation operation = db.Operations.Find(id);
-            if (operation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(operation);
+            Category category = database.Categories.Find(id);
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
+            return View(category);
         }
 
-        // GET: Operation/Create
         public ActionResult Create()
         {
-            ViewBag.Id = new SelectList(db.Categories, "Id", "Name");
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code");
-            return View();
-        }
-
-        // POST: Operation/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Id,Id,Amount,Date,Description")] Operation operation)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Operations.Add(operation);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.Id = new SelectList(db.Categories, "Id", "Name", operation.Id);
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code", operation.Id);
-            return View(operation);
-        }
-
-        // GET: Operation/AddIncome
-        public ActionResult AddIncome()
-        {
-            ViewBag.Id = new SelectList(from x in db.Categories.Where(s => s.Name == "Income") select x, "Id", "Name");
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code");
-            return View();
-        }
-
-        // GET: Operation/AddOutcome
-        public ActionResult AddOutcome()
-        {
-            ViewBag.Id = new SelectList(from x in db.Categories.Where(s => s.Name == "Outcome") select x, "Id", "Name");
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code");
-            return View();
-        }
-
-        // GET: Operation/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Operation operation = db.Operations.Find(id);
-            if (operation == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Id = new SelectList(db.Categories, "Id", "Name", operation.Id);
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code", operation.Id);
-            return View(operation);
-        }
-
-        // POST: Operation/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Id,Id,Amount,Date,Description")] Operation operation)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(operation).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Id = new SelectList(db.Categories, "Id", "Name", operation.Id);
-            ViewBag.Id = new SelectList(db.Currencies, "Id", "Code", operation.Id);
-            return View(operation);
-        }
-
-        // GET: Operation/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Operation operation = db.Operations.Find(id);
-            if (operation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(operation);
-        }
-
-        // POST: Operation/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Operation operation = db.Operations.Find(id);
-            db.Operations.Remove(operation);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            categoriesManager.Equals;            
         }
     }
 }
