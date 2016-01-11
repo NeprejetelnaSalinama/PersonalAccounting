f9f1046f40ffa2243645a6a291b6327d9092e088
Denisa <DenisaVanackova@gmail.com>
2016-01-11 03:22:14 +0100

    HomeController added but not completed yet

--- DenisAccounting/Controllers/HomeController.cs
+++ DenisAccounting/Controllers/HomeController.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Collections;
 using System.Web.Mvc;
+using DenisAccounting.Managers;
+using AutoMapper;
+using DenisAccounting.Models;
+using DenisAccounting.Constants;
+using DenisAccounting.Database;
 
 namespace DenisAccounting.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
-        public ActionResult Index()
+        private OperationsManager operationsManager;
+
+        public HomeController()
         {
-            return View();
+            operationsManager = new OperationsManager(database);
+        }
+
+
+        public ViewResult Home()
+        {
+            const int TOP_OPERATIONS = 10;
+
+            var operations = operationsManager.getTopOperations(TOP_OPERATIONS);
+            var operationsModel = Mapper.Map<Models.Operations.IndexViewModel>(operations);
+
+            var model = new Models.Home.IndexViewModel
+            {
+                Balance = $"{operationsManager.getBalance().ToString()} {Constants.SharedConstants.DEFAULT_CURRENCY}",
+                Operations = new IEnumerable<Models.Operations.IndexViewModel>
+                {
+
+                }
+            };
+    
+
+        return View(model);
         }
     }
 }
